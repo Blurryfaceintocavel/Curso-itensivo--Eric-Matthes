72b56f6e9e3569d9da354868a0302cb49290d6e7
Arih <95106440+Blurryfaceintocavel@users.noreply.github.com>
2026-03-12 00:45:10 -0300

    Fatiamento

---
+++ Listas/#Fatiando uma lista.py
@@ -0,0 +1,12 @@
+#Fatiando uma lista
+
+times= [ 'Flamengo', 'Vasco', 'Botafogo', 'Fluminense', 'Bangu', 'Madureira']
+
+print (times [0:3])
+
+print("---------------")
+
+print (times [1:-2])
+
+for tim in times [:2]:
+    print (tim)
